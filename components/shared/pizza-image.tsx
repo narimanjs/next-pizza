9634ccc49659d8a8d1e617c93ec56490c4a5df63
Nariman <mirmanov1@mail.ru>
2024-08-01 01:54:00 +0500

    fixed import cn in pizza-image.tsx

--- components/shared/pizza-image.tsx
+++ components/shared/pizza-image.tsx
@@ -1,5 +1,5 @@
-import { cn } from '@/shared/lib/utils';
-import React from 'react';
+import { cn } from "@/lib/utils";
+import React from "react";
 
 interface Props {
   className?: string;
@@ -9,19 +9,27 @@ interface Props {
 
 export const PizzaImage: React.FC<Props> = ({ imageUrl, size, className }) => {
   return (
-    <div className={cn('flex items-center justify-center flex-1 relative w-full', className)}>
+    <div
+      className={cn(
+        "flex items-center justify-center flex-1 relative w-full",
+        className
+      )}
+    >
       <img
         src={imageUrl}
-        alt="Logo"
-        className={cn('relative left-2 top-2 transition-all z-10 duration-300', {
-          'w-[300px] h-[300px]': size === 20,
-          'w-[400px] h-[400px]': size === 30,
-          'w-[500px] h-[500px]': size === 40,
-        })}
+        alt='Logo'
+        className={cn(
+          "relative left-2 top-2 transition-all z-10 duration-300",
+          {
+            "w-[300px] h-[300px]": size === 20,
+            "w-[400px] h-[400px]": size === 30,
+            "w-[500px] h-[500px]": size === 40,
+          }
+        )}
       />
 
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed border-2 rounded-full border-gray-200 w-[450px] h-[450px]" />
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dotted border-2 rounded-full border-gray-100 w-[370px] h-[370px]" />
+      <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed border-2 rounded-full border-gray-200 w-[450px] h-[450px]' />
+      <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-dotted border-2 rounded-full border-gray-100 w-[370px] h-[370px]' />
     </div>
   );
 };
