ff111c281fa2235fe84cb04f75bfc27b8277023e
Nariman <mirmanov1@mail.ru>
2024-08-01 01:39:44 +0500

    fixed route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,6 +2,6 @@ import { prisma } from "@/prisma/prisma-client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const users = await prisma.user.findMany();x
+  const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
